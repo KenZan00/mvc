9f501e164c89ab260801cd7322a002422586caf8
Kenny Åkerup <keak23@student.bth.se>
2024-04-11 12:09:30 +0200

    Upload for kmom01

---
+++ README.md
@@ -0,0 +1,12 @@
+# My Awesome Report
+
+![Logo](/assets/images/mvc.jpeg)
+
+## MVC repository
+This is my report for MVC course
+
+## Clone
+To clone repository to a local environment:
+
+1. Clone repository:
+   git clone https://github.com/KenZan00/mvc.git
--- src/Controller/LuckyControllerJson.php
+++ src/Controller/LuckyControllerJson.php
@@ -25,4 +25,45 @@ class LuckyControllerJson
             $response->getEncodingOptions() | JSON_PRETTY_PRINT
         );
         return $response;
-    }}
+    }
+
+    #[Route("/api")]
+    public function api(): Response
+    {
+        
+        $data = [
+            '/api/lucky/number' => 'Get a lucky number',
+            '/api/quote' => 'Get 1 of 3 random quotes'
+        ];
+
+       
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/quote")]
+    public function quote(): Response
+    {
+        
+        $data = [
+            "Det omöjliga tar bara lite längre tid - Sir Winston Churchill",
+            "Förnuftet är en tjänare, intuitionen är en gåva – Einstein",
+            "Det finns inget försöka, det finns bara göra eller inte göra – Yoda"
+
+            // Add more routes as needed
+        ];
+
+        $randomDataIndex = array_rand($data);
+        $randomData = $data[$randomDataIndex];
+
+       
+        $response = new JsonResponse($randomData);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
